587d88b5751e1eee0e79900315a35b1e922527ea
sukggom <sukggom@naver.com>
2025-10-20 16:26:44 +0900

    Add files via upload

---
+++ AssetBundle/UDatabase.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UG.Framework
+{
+    public class UDatabase : UManager<UDatabase>
+    {
+        private Dictionary<string, IDataRecord> Records 
+            = new Dictionary<string, IDataRecord>();
+
+        private UJSONLoader JsonLoader = new UJSONLoader();
+
+        private UScriptableLoader ScriptableLoader = new UScriptableLoader();
+
+        private List<IDataRecord> Preloads = new List<IDataRecord>();
+
+        public override void Initialize()
+        {
+        }
+
+        public override void UnInitialize()
+        {
+            this.ClearPreloads();
+        }
+
+        public override void Update(float InDeltaTime)
+        {
+        }
+
+        public override void ChangingScene()
+        {
+        }
+        public override void ChangedScene()
+        {
+        }
+
+        public void Preload<TValue>(UDataRecord<TValue> InTrunk) where TValue : class
+        {
+            if (Preloads.Contains(InTrunk))
+            {
+                ULogger.Error($"Preload failed. Path: {InTrunk.GetType().FullName}");
+
+                return;
+            }
+            Preloads.Add(InTrunk);
+        }
+
+        public void ClearPreloads()
+        {
+            for(int i = 0; i < Preloads.Count; ++i)
+            {
+                if(Preloads[i].GetRefCount() != 1)
+                {
+                    ULogger.Error($"Database Preload RefCount Error. {Preloads[i].GetPath()} / {Preloads[i].GetRefCount()}");
+                }
+                else
+                {
+                    (Preloads[i] as IDisposable).Dispose();
+                }
+            }
+
+            Preloads.Clear();
+        }
+
+        public UDataRecord<TValue> LoadJsonDatabase<TValue>(string InPath) where TValue : class
+        {
+            UDataRecord<TValue> Trunk = null;
+
+            IDataRecord TmpTrunk;
+            if (Records.TryGetValue(InPath, out TmpTrunk))
+            {
+                Trunk = TmpTrunk as UDataRecord<TValue>;
+                if (null == Trunk)
+                {
+                    ULogger.Error($"LoadDatabase Typecast Error. Path: {InPath}");
+
+                    return null;
+                }
+            }
+            else
+            {
+                Trunk = JsonLoader.Load<TValue>(InPath);
+
+                if (null == Trunk)
+                {
+                    ULogger.Error($"LoadDatabase Load Error. Path: {InPath}");
+
+                    return null;
+                }
+
+                Records.Add(InPath, Trunk);
+            }
+
+            Trunk.IncreaseRef(UnloadDatabase);
+
+            return Trunk;
+        }
+
+        public UDataRecord<TValue> LoadScriptableDatabase<TValue>(string InPath) where TValue : class
+        {
+            UDataRecord<TValue> Trunk = null;
+
+            IDataRecord TmpTrunk;
+            if (Records.TryGetValue(InPath, out TmpTrunk))
+            {
+                Trunk = TmpTrunk as UDataRecord<TValue>;
+                if (null == Trunk)
+                {
+                    ULogger.Error($"LoadDatabase Typecast Error. Path: {InPath}");
+
+                    return null;
+                }
+            }
+            else
+            {
+                Trunk = ScriptableLoader.Load<TValue>(InPath);
+                
+                if (null == Trunk)
+                {
+                    ULogger.Error($"LoadDatabase Load Error. Path: {InPath}");
+
+                    return null;
+                }
+                
+                Records.Add(InPath, Trunk);
+            }
+
+            Trunk.IncreaseRef(UnloadDatabase);
+            
+            return Trunk;
+
+        }
+        private void UnloadDatabase(IDataRecord InTrunk)
+        {
+            bool IsZeroRef = InTrunk.DecreaseRef();
+            if (IsZeroRef)
+            {
+                InTrunk.DestroyAssetRef(); //unload resource
+
+                Records.Remove(InTrunk.GetPath());
+            }
+        }
+    }
+}
---
+++ AssetBundle/UObjectPool.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections.Generic;
+
+namespace UG.Framework
+{
+
+    public class UObjectPool<T> : IPool where T : class, IPoolObject
+    {
+        public interface IAllocator
+        {
+            public T Alloc();
+            public void Release();
+        }
+
+        protected Queue<T> Objects = new Queue<T>();
+        protected IAllocator Allocator = null;
+        protected int AllocCount = 1;
+        protected int TotalCount = 0;
+
+        protected System.Action<T> RecycleAction = null;
+
+#if UG_DEBUG_OBJECTPOOL
+        private Dictionary<T, string> DebugTrace = new Dictionary<T, string>();
+#endif
+        public UObjectPool(int InAllocCount = 1)
+        {
+            AllocCount = InAllocCount;
+        }
+
+        public virtual void Free(IPoolObject InObject)
+        {
+            if (InObject is T)
+            {
+                this.Free(InObject as T);
+            }
+            else
+            {
+                ULogger.Error($"ObjectPool Free Error. bad type. {typeof(T)}");
+            }
+        }
+
+        public virtual void Release()
+        {
+            if(TotalCount != Objects.Count)
+            {
+                ULogger.Error($"ObjectPool Count Error. TotalCount: {TotalCount}, CurrentCount: {Objects.Count}");
+            }
+
+            var ObjectEnumerator = Objects.GetEnumerator();
+            while(ObjectEnumerator.MoveNext())
+            {
+                ObjectEnumerator.Current.OnLastRelease();
+            }
+
+            Objects.Clear();
+
+            TotalCount = 0;
+
+#if UG_DEBUG_OBJECTPOOL
+            ShowDebugTrace();
+#endif
+
+            Allocator?.Release();
+            Allocator = null;
+        }
+
+        public void SetAllocator(IAllocator InAllocator)
+        {
+            Allocator = InAllocator;
+        }
+
+        public void SetRecycleAction(System.Action<T> InRecycleAction)
+        {
+            RecycleAction = InRecycleAction;
+        }
+        public virtual T Alloc()
+        {
+            if(Objects.Count <= 0)
+            {
+                if( false == AddCount(AllocCount))
+                {
+                    return null;
+                }
+            }
+
+            T Object = Objects.Dequeue();
+
+            Object.OnAlloc();
+#if UG_DEBUG_OBJECTPOOL
+            DebugTrace.Add(Object, UnityEngine.StackTraceUtility.ExtractStackTrace());
+#endif
+            
+#if UG_DEBUG_OBJECTS_COUNT
+            UObjectCounterProfiler.DrawStateInfo(string.Format("[Pooling {0}]",typeof(T).ToString()), TotalCount);
+#endif
+            
+            return Object;
+        }
+
+        public virtual void Free(T InObject)
+        {
+            InObject.OnFree();
+
+            Objects.Enqueue(InObject);
+
+#if UG_DEBUG_OBJECTPOOL
+            DebugTrace.Remove(InObject);
+#endif
+#if UG_DEBUG_OBJECTS_COUNT
+            UObjectCounterProfiler.DrawStateInfo(string.Format("[Pooling {0}]",typeof(T).ToString()), TotalCount);
+#endif
+        }
+
+        public bool AddCount(int InCount)
+        {
+            for (int i = 0; i < InCount; ++i)
+            {
+                T Object = null == Allocator 
+                    ? System.Activator.CreateInstance<T>()
+                    : Allocator.Alloc();
+
+                if (null == Object)
+                {
+                    ULogger.Error($"UObjectPool Alloca failed. Type: {typeof(T).FullName}");
+
+                    return false;
+                }
+
+                Object.OnFirstCreate();
+
+                Objects.Enqueue(Object);
+
+                TotalCount += InCount;
+
+            }
+
+            return true;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Objects.GetEnumerator();
+        }
+
+#if UG_DEBUG_OBJECTPOOL
+
+        private void ShowDebugTrace()
+        {
+            if (DebugTrace.Count > 0)
+            {
+                string TraceType = typeof(T).FullName;
+
+                ULogger.Error($"ObjectPool DebugTrace Error. [{TraceType}]");
+                var Enumerator = DebugTrace.GetEnumerator();
+                while (Enumerator.MoveNext())
+                {
+                    ULogger.Error($"-- DebugTrace TraceType: [{TraceType}], StackTrace: [{Enumerator.Current.Value}]");
+                }
+
+                DebugTrace.Clear();
+            }
+        }
+
+     
+     
+#endif
+
+    }
+}
